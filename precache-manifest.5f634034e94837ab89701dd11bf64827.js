f1060790345c1ac107222fa6219df62032de06c2
Ahmed Magdy <madimagdy@gmail.com>
2019-09-02 18:03:28 +0200

    Updates

---
+++ precache-manifest.5f634034e94837ab89701dd11bf64827.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70af19c87d1afb92dff94b359bc7b3d8",
+    "url": "/fendprep-react/index.html"
+  },
+  {
+    "revision": "6a8d730de971fac5f0f1",
+    "url": "/fendprep-react/static/css/main.279d9bcb.chunk.css"
+  },
+  {
+    "revision": "b193c362494796d4f2d8",
+    "url": "/fendprep-react/static/js/2.7b62ede7.chunk.js"
+  },
+  {
+    "revision": "6a8d730de971fac5f0f1",
+    "url": "/fendprep-react/static/js/main.d028c649.chunk.js"
+  },
+  {
+    "revision": "770b6adabca6eddd1266",
+    "url": "/fendprep-react/static/js/runtime~main.852328e2.js"
+  }
+]);
